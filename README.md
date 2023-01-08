ff15b723c7cf534644b7c7142a771589a1f72443
Alex Mendes <alexanderhmendes@gmail.com>
2023-01-08 00:11:44 +0000

    feat: add inital boilerplate

---
+++ README.md
@@ -0,0 +1,21 @@
+# Total Rehab admin
+
+The admin interface for the Total Rehab app, built using
+[Next.js](https://nextjs.org/docs) and [React Admin](https://marmelab.com/react-admin/),
+with a custom data provider that integrates with the Total Rehab API.
+
+## Getting Started
+
+Install dependencies:
+
+```text
+yarn install
+```
+
+Run the development server:
+
+```bash
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000).
---
+++ bin/prepare-env.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+const core = require('@actions/core');
+const Handlebars = require('handlebars');
+const yargs = require('yargs');
+const { hideBin } = require('yargs/helpers');
+
+const { argv } = yargs(hideBin(process.argv));
+const requiredVars = ['appEnv', 'appVersion'];
+
+requiredVars.forEach((requiredVar) => {
+  if (!(requiredVar in argv)) {
+    throw new Error(
+      `The --${requiredVar} argument is required to prepare the app spec.`,
+    );
+  }
+});
+
+const digitalOceanDir = path.join(__dirname, '..', '.do');
+const templatePath = path.join(digitalOceanDir, 'app.template.yaml');
+const appSpecPath = path.join(digitalOceanDir, 'app.yaml');
+const source = fs.readFileSync(templatePath).toString();
+const template = Handlebars.compile(source);
+const compiledContent = template({
+  appEnv: argv.appEnv,
+  appVersion: argv.appVersion,
+});
+
+// Write the app spec
+fs.writeFileSync(appSpecPath, Buffer.from(compiledContent));
+
+// Set the DigitalOcean app ID for use in later GH Actions steps
+core.setOutput(
+  'digitalocean-app-id',
+  process.env[`${argv.appEnv.toUpperCase()}_DIGITALOCEAN_APP_ID`],
+);
