2affb91776804c8a9bf08bb010235c7ec5462c00
Alex Mendes <alexanderhmendes@gmail.com>
2023-01-16 01:00:11 +0000

    feat: update activity inputs

--- src/components/activities/ActivityInputs.tsx
+++ src/components/activities/ActivityInputs.tsx
@@ -1,10 +1,23 @@
-import { TextArrayInput } from '@jambff/ra-components';
+import { MediaLibraryInput, TextArrayInput } from '@jambff/ra-components';
 import { FC } from 'react';
-import { required, TextInput } from 'react-admin';
+import {
+  AutocompleteArrayInput,
+  ReferenceArrayInput,
+  required,
+  TextInput,
+} from 'react-admin';
+import { FlexRow } from '../generic/FlexRow';
 
 export const ActivityInputs: FC = () => (
   <>
     <TextInput source="title" validate={[required()]} fullWidth />
+    <FlexRow>
+      <MediaLibraryInput source="heroImageId" label="Hero Image" />
+      <MediaLibraryInput source="thumbnailImageId" label="Thumbnail Image" />
+    </FlexRow>
     <TextArrayInput source="instructions" />
+    <ReferenceArrayInput source="equipment" reference="equipment">
+      <AutocompleteArrayInput fullWidth label="Equipment" optionText="name" />
+    </ReferenceArrayInput>
   </>
 );
