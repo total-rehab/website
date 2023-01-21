21a33c9732e56ed5a72e085e32a4de096fdc0c27
Alex Mendes <alexanderhmendes@gmail.com>
2023-01-21 23:18:59 +0000

    feat: add description to taxos

--- src/components/taxonomies/TaxonomyInputs.tsx
+++ src/components/taxonomies/TaxonomyInputs.tsx
@@ -2,5 +2,8 @@ import { FC } from 'react';
 import { required, TextInput } from 'react-admin';
 
 export const TaxonomyInputs: FC = () => (
-  <TextInput source="name" validate={[required()]} fullWidth />
+  <>
+    <TextInput source="name" validate={[required()]} fullWidth />
+    <TextInput source="description" fullWidth />
+  </>
 );
