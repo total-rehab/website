56c0e075b1918614a64f78c08f59c1db4b6af371
Alex Mendes <alexanderhmendes@gmail.com>
2023-03-12 00:36:51 +0000

    feat: add facebook domain verification code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export const Document: NextPage = () => (
   <Html lang="en">
-    <Head />
+    <Head>
+      <meta
+        name="facebook-domain-verification"
+        content="bhzgm3ubqcf2wjigbss8co7l18etpg"
+      />
+    </Head>
     <body>
       <Main />
       <NextScript />
