5b38297e1d9902b5faac818fb3692f394dc53760
Alex Mendes <alexanderhmendes@gmail.com>
2023-04-02 10:52:10 +0100

    build: fix blurry placeholder images

--- next.config.js
+++ next.config.js
@@ -11,6 +11,8 @@ const nextConfig = {
   transpilePackages: ['next-image-export-optimizer'],
   images: {
     loader: 'custom',
+    imageSizes: [10, 16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [
       {
         protocol: 'https',
