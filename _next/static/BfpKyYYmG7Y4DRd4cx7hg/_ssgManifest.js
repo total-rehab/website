90a0ed42d0602cbbd5567973ce69729c9a69a93d
alexandermendes <alexander.mendes@immediate.co.uk>
2023-04-09 13:19:33 +0000

    Deploying to gh-pages from @ total-rehab/website@f86babac65a6643090f3db2016c0d2bd3f068b8f 🚀

---
+++ _next/static/BfpKyYYmG7Y4DRd4cx7hg/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblog-post\u002F[id]","\u002Fblog-posts\u002F[page]","\u002Fpolicies\u002Fterms","\u002Fpractitioner\u002Fpurchase","\u002Fprogram\u002F[id]","\u002Fprograms\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
